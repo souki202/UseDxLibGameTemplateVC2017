d5ebd2dd6f12c719b814262a853fa8f909a16e24
Iwatani Souki <TotoriTori@outlook.com>
2017-05-15 12:52:14 +0900

    バグ修正

--- game/Random.h
+++ game/Random.h
@@ -1,11 +1,16 @@
 #pragma once
 #include <random>
+#define rand Random::getInstance()
 
 class Random {
-public:
+private:
 	Random();
 	~Random();
+
+public:
+	static Random& getInstance() { static Random r; return r; };
 	int generate(int min, int max);
+
 private:
 	static std::random_device rnd;
 	static std::mt19937 mt;
